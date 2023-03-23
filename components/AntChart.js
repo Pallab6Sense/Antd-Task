17ad8d48bf7c1ac7aa82811e8d5a4139a661f32a
Pallab6Sense <pallab@6sensehq.com>
2023-03-23 19:17:03 +0600

    Need to fix chart note showing in mobile 7:15 PM

--- components/AntChart.js
+++ components/AntChart.js
@@ -1,46 +1,49 @@
-import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { Column } from '@ant-design/plots';
-
-const DemoColumn = () => {
-  const data = [
-    {
-      type: '01',
-      sales: 850,
-    },
-    {
-      type: '02',
-      sales: 600,
-    },
-    {
-      type: '03',
-      sales: 500,
-    },
-    {
-      type: '04',
-      sales: 620,
-    },
-    {
-      type: '05',
-      sales: 900,
-    },
-    {
-      type: '06',
-      sales: 500,
-    },
-    {
-      type: '07',
-      sales: 900,
-    },
-    {
-      type: '08',
-      sales: 630,
-    },
-    {
-      type: '09',
-      sales: 900,
-    }
-  ];
+// import { Column } from '@ant-design/plots';
+import dynamic from 'next/dynamic';
+const Column = dynamic(
+  () => import('@ant-design/plots').then(({ Column }) => Column),
+  { ssr: false }
+);
+const data = [
+  {
+    type: '01',
+    sales: 850,
+  },
+  {
+    type: '02',
+    sales: 600,
+  },
+  {
+    type: '03',
+    sales: 500,
+  },
+  {
+    type: '04',
+    sales: 620,
+  },
+  {
+    type: '05',
+    sales: 900,
+  },
+  {
+    type: '06',
+    sales: 500,
+  },
+  {
+    type: '07',
+    sales: 900,
+  },
+  {
+    type: '8',
+    sales: 630,
+  },
+  {
+    type: '9',
+    sales: '900',
+  },
+];
+const AntChart = () => {
   const config = {
     data,
     xField: 'type',
@@ -50,24 +53,99 @@ const DemoColumn = () => {
         autoHide: true,
         autoRotate: false,
       },
-      
     },
     columnStyle: {
-    radius: [4, 4, 0, 0],
-  },
-    color: '#2266FD',
-    
-
+      radius: [20, 20, 20, 20],
+    },
+    height: 300,
+    width: 700,
+    label: {
+      style: {
+        color: '#FFFFFF', // set the text color to gray
+      },
+    },
+    color: 'white',
     meta: {
-      
+      type: {
+        alias: '类别',
+      },
       sales: {
-        alias: 'sales',
+        alias: '销售额',
       },
     },
     minColumnWidth: 20,
     maxColumnWidth: 20,
   };
-  return <Column {...config} />;
-};
+  const configMobile = {
+    data,
+    xField: 'type',
+    yField: 'sales',
+    xAxis: {
+      label: {
+        autoHide: true,
+        autoRotate: false,
+      },
+    },
+    columnStyle: {
+      radius: [20, 20, 20, 20],
+    },
+    height: 190,
+    width: 270,
+    label: {
+      style: {
+        color: '#FFFFFF', // set the text color to gray
+      },
+    },
+    color: 'white',
+    meta: {
+      type: {
+        alias: '类别',
+      },
+      sales: {
+        alias: '销售额',
+      },
+    },
+    minColumnWidth: 20,
+    maxColumnWidth: 20,
+  };
+  return (
+    <>
+      <div className="antChart">
+        <Column {...config} />
+      </div>
+      <div className="antChartMobile">
+        <Column {...configMobile} />
+      </div>
+      <div className="chartDesc">
+        <p style={{ fontWeight: 'bold', margin: '8px 0px' }}>Active Users</p>
+        <p style={{ color: '#999898', fontWeight: '600', margin: '8px 0px' }}>
+          than last week <span style={{ color: '#52c41a' }}>+23%</span>{' '}
+        </p>
+        <p style={{ color: '#999898' }}>
+          We have created multiple options for you to put together and customise
+          into pixel perfect pages.
+        </p>
+      </div>
+      <div className="bottomDesc">
+        <div>
+          <h1>3,6K</h1>
+          <p>Users</p>
+        </div>
 
-ReactDOM.render(<DemoColumn />, document.getElementById('container'));
+        <div>
+          <h1>3,6K</h1>
+          <p>Users</p>
+        </div>
+        <div>
+          <h1>3,6K</h1>
+          <p>Users</p>
+        </div>
+        <div>
+          <h1>3,6K</h1>
+          <p>Users</p>
+        </div>
+      </div>
+    </>
+  );
+};
+export default AntChart;
